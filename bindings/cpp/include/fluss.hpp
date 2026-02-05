f06e7aa4978b9b997afd798b420b862b278e3598
Anton Borisov <anton.borisov@fresha.com>
2026-02-05 16:28:11 +0000

    [TASK-240] Fire-and-forget behaviour for effecient batching

--- bindings/cpp/include/fluss.hpp
+++ bindings/cpp/include/fluss.hpp
@@ -37,6 +37,7 @@ namespace ffi {
     struct Admin;
     struct Table;
     struct AppendWriter;
+    struct WriteResult;
     struct LogScanner;
 }  // namespace ffi
 
@@ -404,6 +405,7 @@ struct LakeSnapshot {
 };
 
 class AppendWriter;
+class WriteResult;
 class LogScanner;
 class Admin;
 class Table;
@@ -526,6 +528,30 @@ private:
     std::vector<size_t> projection_;
 };
 
+class WriteResult {
+public:
+    WriteResult() noexcept;
+    ~WriteResult() noexcept;
+
+    WriteResult(const WriteResult&) = delete;
+    WriteResult& operator=(const WriteResult&) = delete;
+    WriteResult(WriteResult&& other) noexcept;
+    WriteResult& operator=(WriteResult&& other) noexcept;
+
+    bool Available() const;
+
+    /// Wait for server acknowledgment of the write.
+    /// For fire-and-forget, simply let the WriteResult go out of scope.
+    Result Wait();
+
+private:
+    friend class AppendWriter;
+    WriteResult(ffi::WriteResult* inner) noexcept;
+
+    void Destroy() noexcept;
+    ffi::WriteResult* inner_{nullptr};
+};
+
 class AppendWriter {
 public:
     AppendWriter() noexcept;
@@ -539,6 +565,7 @@ public:
     bool Available() const;
 
     Result Append(const GenericRow& row);
+    Result Append(const GenericRow& row, WriteResult& out);
     Result Flush();
 
 private:
