02447a1b8591c86270a2fdb653d36fc91e395713
Prajwal-banakar <prajwal.banakara@gmail.com>
2026-03-06 09:46:03 +0000

    adressed review

--- bindings/cpp/src/ffi_converter.hpp
+++ bindings/cpp/src/ffi_converter.hpp
@@ -61,7 +61,6 @@ inline ffi::FfiConfig to_ffi_config(const Configuration& config) {
     ffi_config.scanner_log_fetch_wait_max_time_ms = config.scanner_log_fetch_wait_max_time_ms;
     ffi_config.scanner_log_fetch_max_bytes_for_bucket = config.scanner_log_fetch_max_bytes_for_bucket;
     ffi_config.writer_batch_timeout_ms = config.writer_batch_timeout_ms;
-    ffi_config.writer_batch_timeout_ms = config.writer_batch_timeout_ms;
     ffi_config.connect_timeout_ms = config.connect_timeout_ms;
     ffi_config.security_protocol = rust::String(config.security_protocol);
     ffi_config.security_sasl_mechanism = rust::String(config.security_sasl_mechanism);
